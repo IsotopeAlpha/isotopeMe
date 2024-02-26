2499477223a4e1905bd1cc41d78483af9dfba928
IIsotope <isotopegayina606@gmail.com>
2024-02-26 22:48:37 +0000

    updated

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         <div className='text-3xl text-blue-800 font-bold italic uppercase'>
           <div  className='text-3xl text-blue-800 font-bold italic uppercase font-mono'>Israel Gayina</div>
           <div className='text-lg text-blue-800 font-bold italic font-mono'>
-            Computer Engineer
+            Computer Engineer | Software Developer
           </div>
         </div>
         <div> 
@@ -35,34 +35,58 @@ export default function Home() {
             <td>+233594891672</td>
           </tr>
           <tr>
-            <td className='text-blue-800 font-bold pr-2 flex'><ImWhatsapp color="green"/>Whatsapp Number:</td>
-            <td>+233506260276</td>
-          </tr>
-          <tr>
             <td className='text-blue-800 font-bold pr-2 flex'><ImMail2 color="gray"/>E-mail:</td>
-            <td>israelgayina606@gmail.com, isotopegayina606@gmail.com</td>
+            <td>israelgayina606@gmail.com | isotopegayina606@gmail.com</td>
           </tr>
           <tr>
             <td className='text-blue-800 font-bold pr-2 flex'><ImGit color="indigo"/>Github Porfolio:</td>
             <td><a href='https://github.com/IsotopeAlpha'>https://github.com/IsotopeAlpha</a></td>
+          </tr>
+          <tr>
+            <td className='text-blue-800 font-bold pr-2 flex'><ImGit color="indigo"/>Gitlab Porfolio:</td>
+            <td><a href='https://gitlab.com/IsotopeAlpha'>https://gitlab.com/IsotopeAlpha</a></td>
+          </tr>
+          <tr>
+            <td className='text-blue-800 font-bold pr-2 flex'><ImGit color="indigo"/>LinkedIn Porfolio:</td>
+            <td><a href='https://www.linkedin.com/in/israel-gayina-a90085226'>https://www.linkedin.com/in/israel-gayina-a90085226</a></td>
           </tr>
         </table>
         </div>
       <br />
       <div data-aos="fade-up" data-aos-duration="3000" className='md:w-[80vw] hover: w-[90vw] md:ml-[8vw] md:h-full h-[50vh] overflow-auto bg-white p-5 rounded shadow-md'>
         <div className='text-xl text-blue-800 font-bold italic font-mono uppercase mb-4'><FcIdea/> Overview</div>
-      Detail-oriented, organized, and meticulous employee. Works at a fast pace to meet tight deadlines.
-      Enthusiastic team player ready to contribute to company success. Software Developer is talented at translating customer requirements into tesdiv engineering plans.
-      Maintains exceptional development quality from conception through distribution. Works alongside clients and colleagues through all stages of development to produce outstanding final products.
-      Result-focused software engineering professional with extensive experience in development and testing.
-      Well-versed in promoting objective-oriented approaches to real-time software development. Analytical and detail-oriented. 
-      Hands-on Software Developer is eager to collaborate with customers to find optimal solutions. 
-      Carefully customizes each product to user needs and budgets. Provides reliably actionable code and software assets for customers and internal data libraries. 
-      Enthusiastic technical professional with a complete understanding of the entire software development cycle. A hardworking and passionate job seeker with strong organizational skills eager to secure an entry-level Software Developer position. 
-      Ready to help the team achieve company goals. Very good in troubleshooting and diagnosing computers for hidden faults and finding solutions to them.
-      </div>
+        Detail-oriented, organized, and meticulous employee. Works at a fast pace to meet tight deadlines.
+        Enthusiastic team player ready to contribute to company success. Software Developer is talented at translating customer requirements into tesdiv engineering plans.
+        Maintains exceptional development quality from conception through distribution. Works alongside clients and colleagues through all stages of development to produce outstanding final products.
+        Result-focused software engineering professional with extensive experience in development and testing.
+        Well-versed in promoting objective-oriented approaches to real-time software development. Analytical and detail-oriented. 
+        Hands-on Software Developer is eager to collaborate with customers to find optimal solutions. 
+        Carefully customizes each product to user needs and budgets. Provides reliably actionable code and software assets for customers and internal data libraries. 
+        Enthusiastic technical professional with a complete understanding of the entire software development cycle. A hardworking and passionate job seeker with strong organizational skills eager to secure an entry-level Software Developer position. 
+        Ready to help the team achieve company goals. Very good in troubleshooting and diagnosing computers for hidden faults and finding solutions to them.
+        </div>
       <div data-aos="fade-right" data-aos-duration="2000" className='bg-gray-200 mt-5 p-3 border-rounded md:mr-4 rounded'>
         <div className='text-xl text-blue-800 font-bold italic font-mono uppercase flex'>Work History<FcSupport color="yellow"/></div>
+          <div className='md:text-lg text-blue-800 font-bold italic md:flex mt-4'>
+              <div className='text-black  md:border-[10px]'>2023-04 to Current </div> 	
+              <div className='md:border-[10px]'>Lab Technician & Software Developer</div> 
+              <div className='text-black md:border-[10px]'>Footprint Internationa School, Aflao, Volta, Ghana.</div>    
+           </div>
+          <div>
+            <ul className='list-disc list-inside'>
+              <li>Support computer lab operations.Collaborated with project managers to select ambitious, but realistic coding milestones on pre-release software project development.</li>
+              <li>Support faculty and students to use computer software and hardware.</li>
+              <li>Present on software packages and applications and develop lab templates.</li>
+              <li>Respond to queries and introduce students to use of computers and peripheral equipment.</li>
+              <li>Manage laboratory software, test materials and hardware to ensure security.</li>
+              <li>Handle lab equipment to clean keyboards, monitors and disk drives.</li>
+              <li>Ensure proper treatment and authorized removal of equipment.</li>
+              <li>Log equipment damage and malfunctions.</li>
+              <li>Extend assistance towards computers and other peripherals support condition.</li>
+              <li>Create and maintain school website.</li>
+              <li>Create and maintain other applications needed by the school. </li>
+            </ul>
+          </div>
           <div className='md:text-lg text-blue-800 font-bold italic md:flex mt-4'>
               <div className='text-black  md:border-[10px]'>2021-11 to 2022-08 </div> 	
               <div className='md:border-[10px]'>Software Developer </div> 
@@ -130,9 +154,16 @@ export default function Home() {
             <li>Next Js</li>
             <li>React Js</li>
             <li>Node Js</li>
-            <li>REST API</li>
+            <li>Javascript</li>
+            <li>Typescript</li>
+            <li>Java</li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Tailwind CSS</li>
             <li>SQL</li>
             <li>Mongo DB</li>
+            <li>MVC, MVP and MVVM Design Patterns</li>
+            <li>REST API Development</li>
             <li>Hardware Installation and Troubleshooting</li>
             <li>Software Installation and Troubleshooting</li>
           </ul>
@@ -156,10 +187,13 @@ export default function Home() {
         <div className='text-xl text-blue-800 font-bold italic font-mono uppercase flex'><FcMindMap/> Apps and Sites Built By Me</div>
         <div>
           <ul className='list-disc list-inside'>
-            <li><a href='https://play.google.com/store/apps/details?id=com.iso.mola_app'>Mola - For hostel booking around the University of Cape Coast.</a></li>
-            <li><a href='https://play.google.com/store/apps/details?id=com.mola.hostel_manager_mobile'>Mola Hostel Manager App</a></li>
+            <li><a href='https://isotope-cv.vercel.app/'>My Website</a></li>
+            <li><a href='https://mola-estate-agency-admin-b05ikn5ec-isotopealpha.vercel.app/'>Mola App Admin Site</a></li>
+            <li><a href='https://footprintschool.vercel.app/'>Footprint International School Website</a></li>
             <li><a href='https://sky2cfreight.vercel.app/'>https://sky2cfreight.vercel.app/</a></li>
             <li><a href='https://del-ushering.vercel.app/'>https://del-ushering.vercel.app/</a></li>
+            <li><a href='https://play.google.com/store/apps/details?id=com.iso.mola_app'>Mola App - For hostel booking around the University of Cape Coast.</a></li>
+            <li><a href='https://play.google.com/store/apps/details?id=com.mola.hostel_manager_mobile'>Mola Hostel Manager App</a></li>
           </ul>
         </div>
       </div>
